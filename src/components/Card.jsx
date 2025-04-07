e23647400d5b1a39b7871872cef1be57c0d0c675
soyeon1351 <pack6411351@gmail.com>
2025-04-08 01:02:54 +0900

    카드 리스트 렌더링

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-export default function Card({ item }) {
+export default function Card({item}) {
     //1. item을 받아와서 카드를 렌더링 해야 합니다.
     //2. item의 title, description, category를 각각 h2, p, span태그에 알맞게 넣으세요
     //3. item은 CardList에서 전달됩니다.
@@ -10,9 +10,9 @@ export default function Card({ item }) {
                 <img src={`https://placehold.co/600x400`} />
             </div>
             <div className="card-content">
-                <span className="card-category">???</span>
-                <h2 className="card-title">???</h2>
-                <p className="card-description">???</p>
+                <span className="card-category">{item.category}</span>
+                <h2 className="card-title">{item.title}</h2>
+                <p className="card-description">{item.description}</p>
             </div>
         </div>
     );
